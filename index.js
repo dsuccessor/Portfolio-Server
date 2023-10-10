f6ade7bd0f0b34472823d9d73c77f2f64201d459
Salaudeen Kehinde <salaudeenkr@gmail.com>
2023-10-10 12:02:18 +0100

    Switching from window session to mongodb session

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ const dotenv = require("dotenv");
 dotenv.config();
 const cors = require("cors");
 const app = express();
-const {portfolioRoute} = require("./routes/routes");
-const cookieParser = require("cookie-parser")
-const session = require("express-session")
+const { portfolioRoute } = require("./routes/routes");
+const session = require("express-session");
+const mongoDbStore = require("connect-mongodb-session")(session);
 
 // Mongoose configuration
 mongoose.connect(process.env.MONGO_DB, (error, response) => {
@@ -18,31 +18,47 @@ mongoose.connect(process.env.MONGO_DB, (error, response) => {
   }
 });
 
+// Mongodb Store
+const store = new mongoDbStore(
+  {
+    uri: process.env.MONGO_DB,
+    collection: 'passResetOtp'
+  },
+  //  ((error) => console.log(`Unable to start mongoDBStore ${error}`))
+)
+
+store.on('connection', (result) =>
+  console.log(`Connected to mongodb store ${result}`))
+
+store.on('error', (err) =>
+  console.log(`Connected to mongodb store ${err}`))
+
+
+
 // Middleware
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(cors());
-app.use(cookieParser())
 app.use(
   session({
-    name: 'myOtp', 
-    secret: 'secretkey', 
-    saveUninitialized: true, 
-    resave: true, 
-    cookie: { maxAge: 120000 } }))
+    secret: 'secretkey',
+    saveUninitialized: true,
+    resave: true,
+    cookie: { maxAge: 120000 }, //two minutes
+    store: store,
+  }))
 
 // General Endpoint
-app.use("/portfolio", (req, _, next)=>{
+app.use("/portfolio", (req, _, next) => {
   console.log(req.session)
   return next()
 }, portfolioRoute);
 
 // Server Connection
-app.listen(port, (err) => {   
+app.listen(port, (err) => {
   if (err) {
     console.log("Unable to connect to the express server at port 4000 ", err);
   } else {
     console.log("Connection to the express server on port 4000 established");
   }
 });
-  
